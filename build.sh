f20d7a2a4407af1bf48f82f520899194cb069fef
SpamixOfficial <spamixproducer@gmail.com>
2024-08-07 08:55:10 +0200

    OS can now draw a yello line (and it doesnt crash on boot!)

--- build.sh
+++ build.sh
@@ -2,19 +2,28 @@
 
 # Build our kernel first of all
 echo "Building kernel..."
-cargo build --release
+if [ "$1" == "debug" ]; then
+    cargo build 
+else 
+    cargo build --release
+fi
 echo "Done!"
 # Time to put together an iso!
 # Remove root dir if there was one before
 echo "Creating image..."
 rm -rf image_dir
-mdkir -p image_dir/boot
-cp -v target/x86_64-blames/release/blames image_dir/boot
+mkdir -p image_dir/boot
+if [ "$1" == "debug" ]; then
+    cp -v target/x86_64-blames/debug/blames image_dir/boot
+else 
+    cp -v target/x86_64-blames/release/blames image_dir/boot
+fi
 mkdir -p image_dir/boot/limine
 
 echo "Downloading limine bootloader"
 git clone https://github.com/limine-bootloader/limine.git --branch=v8.x-binary --depth=1
-echo "Installing limine bootloader"
+make -C limine
+echo "Installing limine bootloader to iso"
 cp limine.conf limine/limine-bios.sys limine/limine-bios-cd.bin limine/limine-uefi-cd.bin image_dir/boot/limine
 mkdir -p image_dir/EFI/BOOT
 cp -v limine/BOOTX64.EFI image_dir/EFI/BOOT/
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
 use core::arch::asm;
+use core::panic::PanicInfo;
 use limine::request::FramebufferRequest;
 use limine::BaseRevision;
 
@@ -24,17 +24,63 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     assert!(BASEREVISION.is_supported());
-    let hello_world = b"Hello world!";
-    let vga_buffer = 0xb8000 as *mut u8;
-
+    /*let hello_world = b"Hello world!";
+    let vga_buffer: *mut u8 = match get_video_address() {
+        VideoType::Colour => 0xb8000 as *mut u8,
+        VideoType::Mono => 0xb0000 as *mut u8,
+        VideoType::None => loop {}
+    };
+    
     for (i, &byte) in hello_world.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize *2 + 1) = 0xb;
         }
+    }*/
+    if let Some(framebuffer_response) = FRAMEBUFFER.get_response() {
+        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
+            for i in 0..framebuffer.width() {
+                // Calculate the pixel offset using the framebuffer information we obtained above.
+                // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+                let pixel_offset = 10*framebuffer.pitch() + i*4;
+
+                // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+                unsafe {
+                    *(framebuffer.addr().add(pixel_offset as usize) as *mut u32) = 0xFFF88F00;
+                }
+            }
+        }
     }
 
-    halt_and_clear();
+    loop {}
+    //halt_and_clear();
+}
+
+// Setups ends here, and below this line every function, enum, struct and more should be.
+//
+// Any code which isnt "setup" shall be moved down here!
+
+enum VideoType {
+    None = 0x00,
+    Colour = 0x20,
+    Mono = 0x30,
+}
+
+impl VideoType {
+    fn from_bios_bit_flags(val: u8) -> VideoType {
+        match val {
+            0x20 => VideoType::Colour,
+            0x30 => VideoType::Mono,
+            _ => VideoType::None,
+        }
+    }
+}
+
+fn get_video_address() -> VideoType {
+    unsafe {
+        let bios_bit_flags = 0x410 as *mut u8;
+        return VideoType::from_bios_bit_flags(*bios_bit_flags & 0x30);
+    }
 }
 
 fn halt_and_clear() -> ! {
